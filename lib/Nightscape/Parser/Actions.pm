8aedcb241da98119e20917b340734af35ece976c
Andy Weidenbaum <archbaum@gmail.com>
2015-03-11 22:16:39 -0700

    validate iso_date with Date module

--- lib/Nightscape/Parser/Actions.pm
+++ lib/Nightscape/Parser/Actions.pm
@@ -1,8 +1,14 @@
 use v6;
 class Nightscape::Parser::Actions;
 
-method TOP($/) {
-    # make $/;
+method iso_date($/) {
+    try {
+        my $valid_date = Date.new("$/");
+
+        CATCH {
+            say "Sorry, invalid date";
+        }
+    }
 }
 
 # vim: ft=perl6
