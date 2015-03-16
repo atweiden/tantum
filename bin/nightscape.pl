f50bccfac9d36a6b3c5e6d4b96caf565126bff54
Andy Weidenbaum <archbaum@gmail.com>
2015-03-16 12:43:27 -0700

    beginnings of reading and writing default TOML config

--- bin/nightscape.pl
+++ bin/nightscape.pl
@@ -3,7 +3,26 @@
 use v6;
 use Nightscape;
 
+my $config_dir = "%*ENV<HOME>/.config/nightscape";
+my $config_file = "$config_dir/config.pl";
+
 sub MAIN($file) {
+    # make config directory if it doesn't exist
+    if !$config_dir.IO.d {
+        say "Config directory doesn't exist.";
+        print "Creating config directory in $config_dir… ";
+        mkdir "$config_dir" or die "Sorry, couldn't create directory $config_dir for config. Check permissions?\n$!";
+        say "done.";
+    }
+
+    # write default config file if it doesn't exist
+    if !$config_file.IO.e {
+        print "Placing default config file at $config_file… ";
+        say "done.";
+    }
+
+    # read config options
+
     if $file.IO.e {
         Nightscape.it($file);
     } else {
