1850ee69f700e4d689271db60ba64ca8105bc940
Andy Weidenbaum <archbaum@gmail.com>
2015-03-16 19:49:51 -0700

    revised toml config

--- bin/nightscape.pl
+++ bin/nightscape.pl
@@ -15,17 +15,27 @@ use Nightscape;
 
 # constants
 my $data_dir = "%*ENV<HOME>/.nightscape";
-my $log_dir = "$data_dir/logs"
-my $price_dir = "$data_dir/prices"
+my $log_dir = "$data_dir/logs";
+my $price_dir = "$data_dir/prices";
 my $config_dir = "%*ENV<HOME>/.config/nightscape";
 my $config_file = "$config_dir/config.toml";
 my $config_text = qq:to/EOCONF/;
-log-dir = "$log_dir"
-
-[Currencies]
+[Default]
 base-currency = "USD"
 
+[Personal]
+# this group not needed, since child account exists
+
+[Personal.Bankwest.Cheque]
+# inherits base-currency from Personal:Bankwest, or Personal, or Default
+#open = "2014-01-01 .. *" # optional date range (YYYY-MM-DD .. YYYY-MM-DD)
+
+[Business]
+base-currency = "USD"
+open = "2014-01-02 .. *"
+
 [Currencies.BTC.Prices.USD]
+# duplicate date-price pairs overridden by date-price pairs listed by hand
 price-file = "$price_dir/coindesk-bpi-USD-close.csv"
 "2014-01-01" = 770.4357
 "2014-01-02" = 808.0485
@@ -34,6 +44,7 @@ price-file = "$price_dir/coindesk-bpi-USD-close.csv"
 "2014-01-05" = 940.0972
 "2014-01-06" = 951.3865
 "2014-01-07" = 810.5833
+# price data given in transaction journal overrides configured date-price pairs
 EOCONF
 
 
