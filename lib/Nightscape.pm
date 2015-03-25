5260590db9e19104dce8d64138e3c8bf73e8bf70
Andy Weidenbaum <archbaum@gmail.com>
2015-03-25 11:28:39 -0700

    new way of printing parsed data

--- lib/Nightscape.pm
+++ lib/Nightscape.pm
@@ -7,17 +7,23 @@ method it($file) {
     my $content = slurp $file;
     if my $parsed = Nightscape::Parser.parse($content) {
         for $parsed<journal>.list -> $journal {
-            for $journal<entry>.list -> $entry {
-                say "------------------------------------------------------";
-                for $entry<header>.list -> $header {
-                    say "Date: ", $header<iso_date>;
-                    say "Description: ", $header<description> if $header<description>;
+            for $journal<entry>.kv -> $key, $value {
+                given $key {
+                    when /header/ {
+                        say "------------------------------------------------------";
+                        for $value.list -> $header {
+                            say "Date: ", $header<iso_date>;
+                            say "Description: ", $header<description> if $header<description>;
+                        }
+                    }
+                    when /posting/ {
+                        for $value.list -> $posting {
+                            say "Account: ", $posting<account> if $posting<account>;
+                            say "Transaction: ", $posting<transaction> if $posting<transaction>;
+                        }
+                        say "------------------------------------------------------";
+                    }
                 }
-                for $entry<posting>.list -> $posting {
-                    say "Account: ", $posting<account> if $posting<account>;
-                    say "Transaction: ", $posting<transaction> if $posting<transaction>;
-                }
-                say "------------------------------------------------------";
             }
         }
     }
