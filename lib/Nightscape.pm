399fe23f6eb3b60490e385db0c97160628c0a93f
Andy Weidenbaum <archbaum@gmail.com>
2015-03-26 16:35:59 -0700

    make/made

--- lib/Nightscape.pm
+++ lib/Nightscape.pm
@@ -6,6 +6,17 @@ method it($file) {
     say %Config::CONFIG.perl;
     my $content = slurp $file;
     if my $parsed = Nightscape::Parser.parse($content) {
+        for $parsed.made.list -> $m {
+            say ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;";
+            say $m;
+            say ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;";
+        }
+        for $parsed<journal>.list -> $journal {
+            say "______________________________________________________\n",
+            $journal<entry>.made,
+            "\n______________________________________________________"
+            unless $journal<entry>.made ~~ Nil;
+        }
         for $parsed<journal>.list -> $journal {
             for $journal<entry>.kv -> $key, $value {
                 given $key {
@@ -20,6 +31,9 @@ method it($file) {
                         for $value.list -> $posting {
                             say "Account: ", $posting<account> if $posting<account>;
                             say "Transaction: ", $posting<transaction> if $posting<transaction>;
+                            for $posting<transaction>.list -> $transaction {
+                                say "Exchange Rate: ", $transaction<exchange_rate> if $transaction<exchange_rate>;
+                            }
                         }
                         say "------------------------------------------------------";
                     }
--- lib/Nightscape/Parser/Actions.pm
+++ lib/Nightscape/Parser/Actions.pm
@@ -3,18 +3,61 @@ class Nightscape::Parser::Actions;
 
 method iso_date($/) {
     try {
-        my $valid_date = Date.new("$/");
-
-        CATCH {
-            say "Sorry, invalid date";
-        }
+        make %( iso_date => Date.new("$/") );
+        CATCH { say "Sorry, invalid date"; }
     }
 }
 
+method header($/) {
+    make [ %( header =>
+                %( $<iso_date>».made,
+                    description => substr($<description>, 1, *-1) # description with leading and trailing double quotes removed
+                 )
+            )
+         ];
+}
+
+method account($/) {
+    make [ %( account =>
+                %( account_full => join(':', $<account_main>, $<account_sub>».join(':')),
+                   account_main => $<account_main>,
+                   account_sub => $<account_sub>,
+                   entity => $<account_sub>.list[0]
+                 )
+            )
+         ];
+}
+
 method transaction($/) {
-    #if $<commodity_code> !eq $BASE_COMMODITY_CODE {
-        # check for valid @ syntax || config option linked to valid price data for iso_date
-    #}
+    make [ %( transaction =>
+                %( commodity_minus => $<commodity_minus>,
+                   commodity_symbol => $<commodity_symbol>,
+                   commodity_quantity => $<commodity_quantity>,
+                   commodity_code => $<commodity_code>,
+                   exchange_rate => $<exchange_rate>
+                 )
+            )
+         ];
+}
+
+method posting($/) {
+    make %( $<account>».made,
+            $<transaction>».made
+          );
+}
+
+method entry($/) {
+    make %( $<header>».made,
+            [ $<posting>».made ]
+          );
+}
+
+method journal($/) {
+    make $<entry>».made;
+}
+
+method TOP($/) {
+    make $<journal>».made;
 }
 
 # vim: ft=perl6
