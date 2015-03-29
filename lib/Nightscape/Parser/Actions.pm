10da0c9c92b4dfb24148ebfd8cdf2bfc46d15622
Andy Weidenbaum <archbaum@gmail.com>
2015-03-28 18:13:48 -0700

    improved parsing of grammar action methods

--- lib/Nightscape/Parser/Actions.pm
+++ lib/Nightscape/Parser/Actions.pm
@@ -9,76 +9,87 @@ method iso_date($/) {
 }
 
 method header($/) {
-    make %( iso_date => $<iso_date>».made,
-            $<description> ?? description => substr($<description>, 1, *-1).trim    # description less surrounding double quotes and whitespace
-                           !! description => Nil,                                   # descriptions are optional
-            $<comment> ?? eol_comment => substr($<comment>, 1, *-0).trim            # comment less leading pound symbol and surrounding whitespace
-                       !! eol_comment => Nil                                        # comments are optional
+    make %( header => %( iso_date => $<iso_date>».made,
+                         $<description> ?? description => substr($<description>, 1, *-1).trim
+                                        !! description => Nil,
+                         $<comment> ?? eol_comment => substr($<comment>, 1, *-0).trim
+                                    !! eol_comment => Nil
+                       )
           );
 }
 
 method account($/) {
-    make %( account_full => join(':', $<account_main>, $<entity>, $<account_sub>».join(':')).uc,
-            account_main => $<account_main>.uc,
-            entity => $<entity>.uc,
-            $<account_sub> ?? account_sub => $<account_sub>.list».uc
-                           !! account_sub => Nil
+    make %( account => %( account_full => join(':', $<account_main>, $<entity>, $<account_sub>».join(':')).uc,
+                          account_main => $<account_main>.uc,
+                          entity => $<entity>.uc,
+                          $<account_sub> ?? account_sub => [ $<account_sub>.list».uc ]
+                                         !! account_sub => Nil
+                        )
           );
 }
 
 method exchange_rate($/) {
-    make %( commodity_symbol => $<commodity_symbol>.uc,
-            commodity_quantity => $<commodity_quantity>.abs,
-            commodity_code => $<commodity_code>.uc
+    make %( exchange_rate => %( commodity_symbol => $<commodity_symbol>.uc,
+                                commodity_quantity => $<commodity_quantity>.abs,
+                                commodity_code => $<commodity_code>.uc
+                              )
           );
 }
 
 method transaction($/) {
-    make %( $<commodity_minus> ?? commodity_minus => True
-                               !! commodity_minus => False,
-            commodity_symbol => $<commodity_symbol>.uc,
-            commodity_quantity => $<commodity_quantity>.abs,
-            commodity_code => $<commodity_code>.uc,
-            $<exchange_rate> ?? exchange_rate => $<exchange_rate>».made
-                             !! exchange_rate => Nil
+    make %( transaction => %( $<commodity_minus> ?? commodity_minus => True
+                                                 !! commodity_minus => False,
+                              commodity_symbol => $<commodity_symbol>.uc,
+                              commodity_quantity => $<commodity_quantity>.abs,
+                              commodity_code => $<commodity_code>.uc,
+                              $<exchange_rate> ?? $<exchange_rate>».made
+                                               !! exchange_rate => Nil
+                            )
           );
 }
 
 method posting($/) {
     if $<account> && $<transaction> {
-        make %( account => $<account>».made,
-                transaction => $<transaction>».made,
-                $<comment> ?? eol_comment => substr($<comment>, 1, *-0).trim
-                           !! eol_comment => Nil
+        make %( posting => %( $<account>».made,
+                              $<transaction>».made,
+                              $<comment> ?? eol_comment => substr($<comment>, 1, *-0).trim
+                                         !! eol_comment => Nil
+                            )
               );
     } else {
-        make %( $<comment> ?? posting_comment => substr($<comment>, 1, *-0).trim
-                           !! posting_comment => Nil
+        make %( posting => %( $<comment> ?? posting_comment => substr($<comment>, 1, *-0).trim
+                                         !! posting_comment => Nil
+                            );
               );
     }
 }
 
 method entry($/) {
-    make [
-            %( header => $<header>».made,
-               posting => [ $<posting>».made ]
-             )
-         ];
+    make %( entry => %( $<header>».made,
+                        postings => [ $<posting>».made ]
+                      )
+          );
 }
 
 method journal($/) {
     if $<entry> {
-        make [ %( entry => $<entry>».made ) ];
+        make %( journal => %( entries => [ $<entry>».made ]
+                            )
+              );
     } elsif $<comment> {
-        make [ %( $<comment> ?? comment_line => substr($<comment>, 1, *-0).trim
-                             !! comment_line => Nil ) ];
+        make %( journal => %( comment_line => substr($<comment>, 1, *-0).trim
+                            )
+              );
     } else {
-        make [ %( blank_line => True ) ];
+        make %( journal => %( blank_line => True
+                            )
+              );
     }
 }
 
 method TOP($/) {
-    make $<journal>».made;
+    make %( journals => $<journal>».made
+          );
 }
 
 # vim: ft=perl6
