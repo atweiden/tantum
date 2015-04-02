9d28c9ff255d717b36791279912f52b4a56f4c0c
Andy Weidenbaum <archbaum@gmail.com>
2015-04-02 12:41:11 -0700

    methods for creating Silo and DrCr enums: mksilo, mkdrcr

--- lib/Nightscape/Journal/Entry/Posting.pm
+++ lib/Nightscape/Journal/Entry/Posting.pm
@@ -19,4 +19,21 @@ has $.entity;
 has @.subaccount;
 has %.amounts;
 
+method mksilo(Str $str) returns Silo {
+    my Silo %silo = "ASSETS" => ASSETS,
+                    "EXPENSES" => EXPENSES,
+                    "INCOME" => INCOME,
+                    "LIABILITIES" => LIABILITIES,
+                    "EQUITY" => EQUITY;
+    return %silo{$str};
+}
+
+method mkdrcr(Bool $commodity_minus) returns DrCr {
+    if $commodity_minus {
+        return DEBIT;
+    } else {
+        return CREDIT;
+    }
+}
+
 # vim: ft=perl6
