9a5dc3a8b01b4eec996335bbe84014d9839657f8
Andy Weidenbaum <archbaum@gmail.com>
2015-04-25 22:03:39 -0700

    add entities and currencies data
    - new method: ls_entities
      - filters entities from TOML parsed from config file
    - new method: ls_currencies
      - filters currencies from TOML parsed from config file
    - add entities and currencies data to Nightscape::Config
      - for making entities and pricing data given in the config file
        available during transaction journal parse

--- lib/Nightscape/Config.pm
+++ lib/Nightscape/Config.pm
@@ -1,9 +1,39 @@
 use v6;
 class Nightscape::Config;
 
-has $.config_file = "%*ENV<HOME>/.config/nightscape/config.toml";
-has $.data_dir = "%*ENV<HOME>/.nightscape";
-has $.log_dir = "$!data_dir/logs";
-has $.price_dir = "$!data_dir/prices";
+has Str $.config_file = "%*ENV<HOME>/.config/nightscape/config.toml";
+has Str $.data_dir = "%*ENV<HOME>/.nightscape";
+has Str $.log_dir = "$!data_dir/logs";
+has Str $.currencies_dir = "$!data_dir/currencies";
+has %.entities is rw;
+has %.currencies is rw;
+
+# filter entities from unvalidated %toml config
+method ls_entities(%toml) {
+    my Str @entities_found;
+    use Nightscape::Parser::Grammar;
+    %toml.map({
+        if my $parsed_section = Nightscape::Parser::Grammar.parse($_.keys, :rule<account_sub>) {
+            push @entities_found, $parsed_section.orig.Str unless Nightscape::Parser::Grammar.parse($parsed_section.orig, :rule<reserved>);
+        }
+    });
+
+    my %entities_found;
+    for @entities_found -> $entity_found {
+        %entities_found{$entity_found} = %toml{$entity_found};
+    }
+    %entities_found;
+}
+
+# filter currencies from unvalidated %toml config
+method ls_currencies(%toml) {
+    my Str $currencies_header;
+    %toml.map({ $currencies_header = $/.orig.Str if $_.keys ~~ m:i / ^currencies /; });
+    if $currencies_header {
+        return %toml{$currencies_header};
+    } else {
+        return Nil;
+    }
+}
 
 # vim: ft=perl6
