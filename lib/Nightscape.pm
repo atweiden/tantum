9cee01b473b9c1d48baa4f74f7a59872b3401e18
Andy Weidenbaum <archbaum@gmail.com>
2015-05-01 20:39:02 -0700

    gen_conf, gen_txjournal, delegate building of classes to Actions.pm
    - let Actions.pm handle building classes
    - method: gen_conf
      - returns Nightscape::Config
    - method: gen_txjournal
      - returns List[Nightscape::Journal]

--- lib/Nightscape.pm
+++ lib/Nightscape.pm
@@ -3,83 +3,31 @@ use Nightscape::Config;
 use Nightscape::Journal;
 class Nightscape;
 
-has Nightscape::Config $.conf;
-has Nightscape::Journal $.journal;
+has Nightscape::Config $.conf is rw;
+has Nightscape::Journal @.txjournal is rw;
 
-method mkconf(%conf?)
+method gen_conf(%conf?) returns Nightscape::Config
 {
-    $!conf = Nightscape::Config.new(|%conf);
+    Nightscape::Config.new(|%conf);
 }
 
-method mkjournal($file)
+method gen_txjournal(Str $file)
 {
     use Nightscape::Parser;
     if my $parsed = Nightscape::Parser.parse(slurp($file), self.conf)
     {
-        use Nightscape::Journal::Entry;
-        use Nightscape::Journal::Entry::Posting;
-        my @entries;
-        for $parsed.made.list -> $parse
-        {
-            my %header;
-            my @postings;
-            for $parse.kv -> $key, $value
-            {
-                unless $key ~~ / blank_line || comment_line /
-                {
-                    if $value.key ~~ /header/
-                    {
-                        %header<id> = $value.value<id>;
-                        %header<date> = $value.value<iso_date>;
-                        %header<description> = $value.value<description>;
-                        %header<important> = $value.value<important>.abs;
-                        %header<tags> = $value.value<tags>;
-                    }
-                    elsif $value.key ~~ /postings/
-                    {
-                        loop (my $i = 0; $i < $value.value.elems; $i++)
-                        {
-                            unless $value.value[$i]<posting_comment>
-                            {
-                                if $value.value[$i]<account><account_main>
-                                {
-                                    my %posting;
-                                    %posting<silo> = Nightscape::Journal::Entry::Posting.mksilo: $value.value[$i]<account><account_main>;
-                                    %posting<drcr> = Nightscape::Journal::Entry::Posting.mkdrcr: $value.value[$i]<transaction><commodity_minus>;
-                                    %posting<entity> = $value.value[$i]<account><entity>;
-                                    %posting<subaccount> = $value.value[$i]<account><account_sub>;
-                                    %posting<amounts> = %(
-                                        commodity_code => $value.value[$i]<transaction><commodity_code>,
-                                        commodity_quantity => $value.value[$i]<transaction><commodity_quantity>,
-                                        $value.value[$i]<transaction><exchange_rate>
-                                        ?? exchange_rate => %(
-                                            commodity_code => $value.value[$i]<transaction><exchange_rate><commodity_code>,
-                                            commodity_quantity => $value.value[$i]<transaction><exchange_rate><commodity_quantity>
-                                        )
-                                        !! exchange_rate => Nil
-                                    );
-                                    push @postings,
-                                        Nightscape::Journal::Entry::Posting.new(
-                                            |%posting
-                                        );
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            push @entries, Nightscape::Journal::Entry.new(
-                |%header,
-                postings => @postings
-            ) unless $parse.kv.pairs[0].value ~~ / blank_line || comment_line /;
-        }
-        $!journal = Nightscape::Journal.new(
-            entries => @entries.sort({
-                $^b.important > $^a.important
-            }).sort({
-                .date
-            })
-        );
+        # filter entries, sort by importance descending then by date ascending
+        $parsed.made.grep({
+            .entry
+        }).sort({
+            $^b.entry.header.important > $^a.entry.header.important
+        }).sort({
+            .entry.header.date
+        });
+    }
+    else
+    {
+        die "Sorry, could not parse transaction journal at 「$file」";
     }
 }
 
