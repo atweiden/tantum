b600395aca939a3d4116f76575957860a47f4c69
Andy Weidenbaum <archbaum@gmail.com>
2015-05-01 20:39:54 -0700

    differentiate journal entries from blank and comment lines
    - Nightscape::Journal can be thought of as a stub
      - unknown whether it is a blank line, a comment line, or an entry

--- lib/Nightscape/Journal.pm
+++ lib/Nightscape/Journal.pm
@@ -2,6 +2,8 @@ use v6;
 use Nightscape::Journal::Entry;
 class Nightscape::Journal;
 
-has Nightscape::Journal::Entry @.entries;
+has Bool $.is_blank_line;
+has Str $.comment_line;
+has Nightscape::Journal::Entry $.entry;
 
 # vim: ft=perl6
