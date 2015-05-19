a0260896b5bf066390be5e9cb94d1e9115711cbb
Andy Weidenbaum <archbaum@gmail.com>
2015-05-19 00:47:20 -0700

    added Nightscape::Entity::Wallet class
    - new attr: balance
      - hash table: Rat indexed by CommodityCode
    - new attr: subwallet
      - hash table: Nightscape::Entity::Wallet indexed by VarName
    - new method: getbalance
      - get wallet balance recursively
    - new method: setbalance
      - debit/credit wallet balance based on values extracted from
        Nightscape::Entry::Posting class

---
+++ lib/Nightscape/Entity/Wallet.pm
@@ -0,0 +1,48 @@
+use v6;
+use Nightscape::Specs;
+class Nightscape::Entity::Wallet;
+
+# balance, indexed by commodity code
+has Rat %.balance{CommodityCode};
+
+# subwallet, indexed by name
+has Nightscape::Entity::Wallet %.subwallet{VarName} is rw;
+
+# get wallet balance, recursively
+method getbalance(CommodityCode $commodity_code) returns Rat
+{
+    my Rat $balance = self.balance{$commodity_code} // 0;
+
+    # is there a subwallet?
+    if self.subwallet
+    {
+        # add subwallet balance to $balance
+        for self.subwallet.kv -> $name, $subwallet
+        {
+            $balance += $subwallet.getbalance($commodity_code);
+        }
+    }
+
+    $balance;
+}
+
+# set wallet balance
+method setbalance(
+    CommodityCode $commodity_code,
+    DrCr $drcr,
+    Quantity $commodity_quantity
+)
+{
+    if $drcr
+    {
+        # credit: increase balance
+        %!balance{$commodity_code} += $commodity_quantity;
+    }
+    else
+    {
+        # debit: decrease balance
+        %!balance{$commodity_code} -= $commodity_quantity;
+    }
+}
+
+# vim: ft=perl6
