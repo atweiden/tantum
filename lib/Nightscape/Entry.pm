7d171949f80e5a7b39cf7e1db0a01663863eadc3
Andy Weidenbaum <archbaum@gmail.com>
2015-06-04 16:36:05 -0700

    implemented exchange rate mismatch detection in Entry.pm method is_balanced
    - updated method: is_balanced
      - entry will not balance without matching exchange rates per asset
      - all postings within an entry affecting a given asset must share the
        same exchange rate

--- lib/Nightscape/Entry.pm
+++ lib/Nightscape/Entry.pm
@@ -23,6 +23,9 @@ method is_balanced(Nightscape::Config $conf) returns Bool
     # running total
     my Rat $total;
 
+    # exchange rate consistency verification
+    my Array[Quantity] %xe_verify{AssetCode};
+
     # entry date
     my Date $date = self.header.date;
 
@@ -38,6 +41,18 @@ method is_balanced(Nightscape::Config $conf) returns Bool
             $date,
             $id
         );
+
+        # is posting denominated in asset other than entity's base
+        # currency?
+        my AssetCode $posting_entity_base_currency =
+            $conf.get_base_currency($posting.account.entity);
+        if $posting.amount.asset_code !eq $posting_entity_base_currency
+        {
+            # store posting exchange rate for comparison with other
+            # postings affecting the same asset
+            push %xe_verify{$posting.amount.asset_code},
+                $posting.amount.exchange_rate.asset_quantity;
+        }
 
         # get posting silo
         my Silo $silo = $posting.account.silo;
@@ -58,6 +73,31 @@ method is_balanced(Nightscape::Config $conf) returns Bool
         {
             # decrease running total
             $total -= $posting_value * $multiplier;
+        }
+    }
+
+    # exchange rate mismatch detection
+    #
+    # if exchange rate does not remain consistent for a given asset
+    # within the span of one entry, exit with an error
+    if %xe_verify
+    {
+        for %xe_verify.kv -> $asset_code, @exchange_rates
+        {
+            unless @exchange_rates.unique.elems == 1
+            {
+                # error: exchange rate mismatch detected
+                my Int $id = self.header.id;
+                die qq:to/EOF/;
+                Sorry, exchange rate for asset 「$asset_code」 does
+                not remain consistent in entry id 「$id」.
+
+                To debug, verify transaction journal entry contains
+                consistent exchange rate. If exchange rate sourced
+                from config, check that configured exchange rate is
+                correct.
+                EOF
+            }
         }
     }
 
